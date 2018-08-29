35372bb02bc9c42bc0885ec6a54c38af3e59f23a
Alec Chan <me@alecchan.io>
2018-08-29 15:10:58 -0600

    Removed dependency on express and simply use http module to serve as an upgrade path for socket.io

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,12 @@
  * Configure and bootstrap server stack
  * Main entrypoint
  */
-import express from 'express';
 import http from 'http';
 import io from 'socket.io';
 import dotenv from 'dotenv';
 import { FirebaseConnector, SnapItem } from './firebaseConnector';
 import SocketRoomContext from './socketRoomContext';
-
+import WSEvents from './wsEvents';
 export class Server {
   public static start() {
     // env vars available through process.env
@@ -17,31 +16,19 @@ export class Server {
     // init our connection to firebase since we've loaded env vars
     FirebaseConnector.initialize();
 
-    const app = express();
-    const server = new http.Server(app);
+    const server = http.createServer().listen(+process.env.PORT, process.env.HOSTNAME);
     const ws = io(server);
     const roomCtxs: SocketRoomContext[] = [];
 
-    app.get('/rooms/create', (req, res) => {
-      const token: string = req.params["token"];
-      const roomId: string = req.params["roomId"];
-      if(roomId === "") res.sendStatus(400); // send bad request due to invalid param
-      FirebaseConnector.verifyUser(token).then((valid) => {
-        if(valid) {
-
-        } else {
-          res.sendStatus(401); // send unauthenticated due to invalid auth token
-        }
-      })
+    // initial landing place for socket connections
+    ws.on('connection', (socket) => {
+      // hook up all our websocket events to this socket connection
+      WSEvents.configure(socket);
     });
 
     // get rooms from database and then setup the connection
     FirebaseConnector.hookRoomCtxFactory((room: SnapItem) => {
       roomCtxs.push(new SocketRoomContext(ws.of(room.key)));
-    });
-
-    server.listen(3000, () => {
-      console.log('listening on *:3000');
     });
   }
 }
