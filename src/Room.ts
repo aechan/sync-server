4a0dcd3dbbb41a216dab089a54a36fef03ca664b
Alec Chan <me@alecchan.io>
2018-11-15 16:29:34 -0600

    Rebased on dev

--- src/Room.ts
+++ src/Room.ts
@@ -1,57 +1,56 @@
-import { Client } from "./Client";
-import { Firebase } from "./Firebase";
-import ws from 'ws';
+/*******************************************************
+ * Copyright (C) 2018 Alec Chan - All Rights Reserved
+ * Unauthorized copying of this file,
+ * via any medium is strictly prohibited
+ * Alec Chan <me@alecchan.io> November, 2018
+ *******************************************************/
 
+import socketIo from 'socket.io';
+import { Client } from './Client';
+
+/**
+ * Manages a single room and it's clients/sockets
+ */
 export class Room {
-    clients: Client[];
-    id: string;
-    fb: Firebase;
-    server: ws.Server;
+    private roomId: string;
+    private room: socketIo.Namespace;
+    private clients: Client[];
 
-    constructor() {
-        this.server = new ws.Server({ noServer: true });
-        this.server.on("connection", (socket: ws) => {
-            this.clients.push(new Client(socket));
-        });
+    private videoURL: string;
+    private ownerUID: string;
+    private imageURL: string;
+    private name: string;
+    private ownerName: string;
+
+    constructor(roomId: string, roomNSP: socketIo.Namespace) {
+        this.roomId = roomId;
+        this.room = roomNSP;
+        this.clients = [];
     }
 
-    get wss() {
-        return this.server;
-    }
-    
-    /**
-     * Removes a client from this room.
-     * @param client client to remove
-     */
-    public removeClient(client: Client) {
-        const found = this.clients.find((c: Client) => c.id === client.id);
-        if(found === undefined) return; // not found in room
-
-        this.fb.removeFromRoom(found);
-        found.room = undefined;
-        
-        // filter out the target client from this 
-        this.clients = this.clients.filter(val => {
-            val.id !== client.id;
-        });
-    }
-
-    /**
-     * Adds a client as a member of this room.
-     * @param client client to add
-     */
-    public addClient(client: Client) {
-        client.room = this;
+    public addClient(client: Client): void {
         this.clients.push(client);
-        this.fb.addToRoom(client);
+        client.socket.emit('JoinedRoom', this.json);
+        client.roomId = this.roomId;
     }
 
-    /**
-     * Checks if the given client is a member of this room.
-     * @param client client to check
-     */
-    public checkMembership(client: Client): boolean {
-        return this.clients.find(s => s.id === client.id) !== undefined;
+    public removeClient(client: Client): void {
+        this.clients = this.clients.filter((val: Client) => {
+            return val.uid !== client.uid;
+        });
+        client.roomId = '';
     }
 
+    get id(): string {
+        return this.roomId;
+    }
+
+    get json(): {} {
+        return {
+            roomId: this.roomId,
+            //room: this.room,
+            //clients: this.clients,
+            videoURL: this.videoURL
+        };
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,114 @@
-import ws from 'ws';
+/*******************************************************
+ * Copyright (C) 2018 Alec Chan - All Rights Reserved
+ * Unauthorized copying of this file,
+ * via any medium is strictly prohibited
+ * Alec Chan <me@alecchan.io> November, 2018
+ *******************************************************/
 
+/**
+ * Configure and bootstrap server stack
+ * Main entrypoint
+ */
+import asciiArt from 'ascii-art';
+import dotenv from 'dotenv';
+import express from 'express';
+import firebaseAdmin from 'firebase-admin';
+import http from 'http';
+import socketIo from 'socket.io';
+import { Client } from './Client';
+import { firebaseConnector } from './FirebaseConnector';
+import { logger } from './logger';
+import { Room } from './Room';
+
+/**
+ * Main server class. Provides interface for start/stop
+ */
 export class Server {
-    
+  private app: express.Express;
+  private server: http.Server;
+  private ws: socketIo.Server;
+  private rooms: Room[];
+  private clients: Client[];
+
+  public start(port?: number, callback?: () => void): void {
+    // env vars available through process.env
+    dotenv.config();
+
+    // init our connection to firebase since we've loaded env vars
+    firebaseConnector.initialize();
+
+    this.app = express();
+    this.server = new http.Server(this.app);
+    this.ws = socketIo(this.server);
+    this.rooms = [];
+
+    // for debug
+    this.app.get('/rooms/:roomId', (req: express.Request, res: express.Response) => {
+      const roomIdKey: string = 'roomId';
+      const room: Room = this.rooms.find((r: Room) => {
+        return r.id === req.param(roomIdKey);
+      });
+      res.send(room.json);
+    });
+
+    this.app.get('/rooms/', (req: express.Request, res: express.Response) => {
+      const token: string = req.param('jwt');
+      firebaseConnector.verifyUser(token).then((val: firebaseAdmin.auth.DecodedIdToken) => {
+        res.send(this.rooms);
+      }).catch((err: string) => {
+        res.send(err);
+      });
+    });
+
+    this.ws.on('connection', (socket: socketIo.Socket) => {
+      socket.on('Authenticate', (token: string) => {
+        firebaseConnector.verifyUser(token).then((val: firebaseAdmin.auth.DecodedIdToken) => {
+            this.clients.push(new Client(val.uid, socket));
+            socket.emit('Authenticated', true);
+        }).catch((err: string) => {
+            logger.info(`Got invalid JWT from socket, error: ${err}`);
+            socket.emit(`AuthError`, `Error authenticating connection: ${err}`);
+            socket.disconnect();
+        });
+      });
+
+      socket.on('JoinRoom', (roomId: string) => {
+        const client: Client = this.clients.find((c: Client) => c.socket.id === socket.id);
+        const room: Room = this.rooms.find((r: Room) => r.id === roomId);
+
+        if (!client) {
+          socket.emit('Unauthenticated', 'Authenticate first.');
+
+          return;
+        }
+
+        logger.info(`Request to join room: ${roomId}`);
+        socket.leaveAll();
+        socket.join(roomId);
+
+        if (!room) {
+          const r: Room = new Room(roomId, this.ws.in(roomId));
+          r.addClient(client);
+          this.rooms.push(r);
+        } else {
+          room.addClient(client);
+        }
+      });
+    });
+
+    this.server.listen(port || process.env.PORT, async () => {
+      // this is so stupidly wasteful but ascii art....
+      logger.info(await asciiArt.font('Sync-Server  v2.0', 'Doom', 'magenta').toPromise());
+      logger.info(`Sync-Server listening on *:${port || process.env.PORT}`);
+    });
+
+    if (callback) {
+      callback();
+    }
+  }
+
+  public stop(callback?: () => void): void {
+    this.server.close(callback);
+  }
+
 }
