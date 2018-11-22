4ffe4dd9b4883aaa4e432b47d1a56b0ed1db222b
Alec Chan <me@alecchan.io>
2018-11-22 14:27:05 -0800

    Refactored all string constants into constants enum

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export enum constants {
+    joined = 'JoinedRoom',
+    sync = 'Sync',
+    owner = 'Owner',
+    chat = 'Chat',
+    roomInfo = 'RoomInfo',
+    stateUpdate = 'StateUpdate',
+    userInfoLoaded = 'UserInfoLoaded'
+}
